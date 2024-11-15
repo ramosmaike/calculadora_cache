cc008c39054e067991f409a4cd431b975846e338
Maike Ramos <maikesystem@gmail.com>
2024-11-15 14:11:28 -0300

    Add files via upload

---
+++ inicio.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Sistema para calculos de horas em cachê")
+st.divider()
+
+st.text("sistema será capaz de fazer calculos para cobrar seu valor de horas em cachê")
+st.text("foi programado em python usando a biblioteca streamlit")
+st.text("PROGRAMADOR:MAIKE RAMOS")
+st.text("email:maikesystem@gmail.com")
+st.divider()
